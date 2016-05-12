dbdf1cd69c8f70c0771fc27c31be4ca29f0fcfbc
OswaldoPV <oswaldo.dsaupoo@gmail.com>
2016-05-12 08:57:15 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -51,4 +51,18 @@ $(document).ready(function (e){
   function escribehistoria(accion){
 	  $('#eHistoria').append('<li>'+accion+'</li>');
   }
+  
+ $(document).ready(function (e){
+	document .addEventListener("deviceready",function(){
+	
+$('#beep').tap(function (){
+	navigator .notification.beep (1);
+});//tap beep
+
+$('#vibrar').tap(function (){
+	navigator .notification.vibrate (1000);
+});//tap vibrar
+
+	},false);//deviceready
+});//ready
 
